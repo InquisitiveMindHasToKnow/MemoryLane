3865f6354b6cb069e542692a740f29e1d61ad8fc
InquisitiveMindHasToKnow <omarraymond18@gmail.com>
2024-07-28 16:50:13 -0400

    Uses @login_required decorator to only allow routes to be visited after login

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
 
 
 # All routes / pages users can navigate to
@@ -6,5 +8,6 @@ views = Blueprint('views', __name__)
 
 # this function runs whenever we go to home page
 @views.route('/')
+@login_required # can't get to home page unless logged in
 def home():
     return render_template("home.html")
