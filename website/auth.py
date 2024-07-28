552accb656b7346a77a199f9268569d1d4d34c7c
InquisitiveMindHasToKnow <omarraymond18@gmail.com>
2024-07-28 15:03:16 -0400

    Adds type of requests auth functions can accept to methods

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 # User auth blueprint 
 auth = Blueprint('auth', __name__)
 
-@auth.route('sign-up')
+@auth.route('sign-up', methods=['GET', 'POST']) #method allows us to decide what kind of request we can accept in the route.
 def sign_up():
-    return render_template('sign_up.html')
-@auth.route('login')
+    data = request.form
+    return render_template('sign_up.html')  
+
+@auth.route('login', methods=['GET', 'POST'])
 def login():
+    data = request.form
     return render_template('login.html')
 
 @auth.route('/logout')
